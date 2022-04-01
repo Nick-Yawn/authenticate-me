58f11c139c27c25cc4c42c6dc2bede06e8be3024
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-04-01 15:16:43 -0500

    add signup thunk, fix other thunks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import { getSessionUser } from './store/session';
 
 require('./App.css');
 
 function App() {
+  const dispatch = useDispatch();
+  // isLoaded state slice, to be updated on loading user, or other fetch calls? 
+ 
+  useEffect( () => {
+    dispatch(getSessionUser());
+  }, [dispatch])
+  
   return (
     <Switch>
       <Route path='/login'>
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -2,29 +2,34 @@ import { csrfFetch } from './csrf.js';
 
 const SET_USER = 'user/SET'
 const UNSET_USER = 'user/UNSET'
+const GET = 'user/GET'
 
-export const set = user => ({
+const set = user => ({
   type: SET_USER,
   user
 })
 
-export const unset = () => ({
+const unset = () => ({
   type: UNSET_USER,
   user: null
 })
 
+const get = user => ({
+  type: GET,
+  user
+}) 
+
 export const setSessionUser = credentials => async dispatch => {
-  console.log(credentials);
    
   const response = await csrfFetch('/api/session', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(credentials)
+    body: JSON.stringify(credentials) // { credential, password }
   });
 
   if( response.ok ){
-    const user = await response.json();
-    dispatch(set(user))
+    const data = await response.json();
+    dispatch(set(data.user))
     return null;
   } else {
     const data = await response.json();
@@ -33,11 +38,41 @@ export const setSessionUser = credentials => async dispatch => {
 
 };
 
+export const getSessionUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session')
+  
+  if( response.ok ){
+    const data = await response.json();
+    dispatch(get(data?.user || null));
+  }
+}
+
+export const signup = credentials => async dispatch => {
+  
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(credentials) // {username, email, password}
+  });
+
+  if( response.ok ){
+    const data = await response.json();
+    dispatch(set(data.user))
+    return null;
+  } else {
+    const data = await response.json();
+    return data?.errors;
+  }
+ 
+}
+
 export default function sessionUserReducer(state = null, action) {
   switch(action.type){
     case SET_USER:
       return {...state, user: action.user}
     case UNSET_USER:
+      return {...state, user: action.user}
+    case GET:
       return {...state, user: action.user}
     default:
       return state
