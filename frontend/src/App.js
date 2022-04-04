cebdd2b59406ab941d19e9c88c3c76fd3f5e5055
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-04-04 12:21:18 -0500

    add nav bar and basic placeholders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
 import { getSessionUser } from './store/session';
 
 require('./App.css');
@@ -10,20 +11,25 @@ require('./App.css');
 function App() {
   const dispatch = useDispatch();
   // isLoaded state slice, to be updated on loading user, or other fetch calls? 
+  const [isLoaded, setIsLoaded ] = useState(false);
  
   useEffect( () => {
-    dispatch(getSessionUser());
+    dispatch(getSessionUser()).then( () => setIsLoaded(true) ) ;
   }, [dispatch])
   
   return (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-      </Route>
-      <Route path='/signup'>
-        <SignupFormPage />
-      </Route>
-    </Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      <Switch>
+        <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+        <Route path='/signup'>
+          <SignupFormPage />
+        </Route>
+      </Switch>
+      <div className="footer" />
+    </>
   );
 }
 
