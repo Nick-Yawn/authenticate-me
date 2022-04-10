4de02112c978687452647c4f0018f54fa0a6fe3a
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-04-09 22:49:32 -0500

    add loading state to SpotsPage

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSpots } from '../../store/spots'
@@ -10,15 +10,22 @@ export default function SpotsPage({select}) {
   const dispatch = useDispatch();  
   const { search }  = useLocation();
   const spots = useSelector( state => state.spots )
-  
+  const [ stateLoaded, setStateLoaded ] = useState(false);
+
+  // this prevents the old state from ghosting the favorites / my-spots / spots page
   useEffect(() => {
-    dispatch(getSpots(select, search));
+    const asyncFunc = async () => {
+      setStateLoaded(false);
+      await dispatch(getSpots(select, search));
+      setStateLoaded(true);
+    }
+    asyncFunc();
   }, [dispatch, select]) 
 
   return (
     <div className="spots-body-content">
       <h1>{ }</h1>
-      {spots && Object.keys(spots).map( key => (
+      {spots && stateLoaded && Object.keys(spots).map( key => (
         <SpotCard spot={spots[key]} key={key} />
       ))}
     </div>
